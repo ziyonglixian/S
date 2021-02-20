63d0b8da238a56ff260a82652fbc40958973aa30
ziyonglixian <68988540+ziyonglixian@users.noreply.github.com>
2021-02-20 23:05:52 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,8 +3,8 @@ import threading
 import requests
 import os
 
-# yzkpht 
-cookie = os.environ["COOKIESS1"]
+# zchfrh 
+cookie = os.environ["COOKIESS2"]
 headers1 = {
     "user-agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
